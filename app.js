e2bf7ba07a724442650321f73b08ae5e9729b035
aqqzbohza <aqqzbohza@gmail.com>
2018-02-20 16:19:36 +0700

    get oid

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-// const bodyParser = require('body-parser')
-// const mongoose = require('mongoose')
 
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(bodyParser.json())
+var snmp = require ("net-snmp")
+var session = snmp.createSession ("192.168.1.1", "private");
+console.log('Server is running on port: ')
+// os, temp, cpu %, mem, in, out
+var oids = [
+  '1.3.6.1.2.1.1.1.0',
+  '1.3.6.1.4.1.9.9.13.1.3.1.3.1008',
+  '1.3.6.1.4.1.9.9.109.1.1.1.1.5.1',
+  '1.3.6.1.4.1.9.9.48.1.1.1.5.1',
+  '1.3.6.1.2.1.2.2.1.10.1',
+  '1.3.6.1.2.1.2.2.1.16.1'
+]
+// .1.3.6.1.2.1.2.2.1.7
+session.get (oids, function (error, varbinds) {
+    if (error) {
+        console.error (error);
+    } else {
+        for (var i = 0; i < varbinds.length; i++)
+            if (snmp.isVarbindError (varbinds[i]))
+                console.error (snmp.varbindError (varbinds[i]))
+            else
+                console.log (varbinds[i].oid + " = " + varbinds[i].value);
+    }
 
-// mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`, { useMongoClient: true })
-const port = process.env.PORT || 9999
-// const Todo = require('./app/models/todo')
+    // If done, close the session
+    session.close ();
+});
 
-app.get('/todos', (request, response) => {
-  // Todo.find().then(todos => {
-  //   response.json({todos})
-  // })
-})
-
-app.listen(port)
-console.log('Server is running on port: ' + port)
+// app.listen(port)
+// console.log('Server is running on port: ' + port)
+1.3.6.1.2.1.2.2.1.8
